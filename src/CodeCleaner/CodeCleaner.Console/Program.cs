c5b60233897753586a99fbbca91e47c693ad0ec2
Sebastian Rumohr <sebastian.rumohr@projektanker.de>
2020-12-14 17:41:47 +0100

    Adds stdin and out pipe support

--- src/CodeCleaner/CodeCleaner.Console/Program.cs
+++ src/CodeCleaner/CodeCleaner.Console/Program.cs
@@ -9,18 +9,83 @@ using Microsoft.CodeAnalysis.Formatting;
 
 namespace CodeCleaner
 {
+    internal enum Mode
+    {
+        Console,
+        File,
+    }
+
     internal class Program
     {
-        private static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            string file = await File.ReadAllTextAsync(args[0]).ConfigureAwait(false);
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(file);
+            Mode mode;
+            if (Console.IsInputRedirected)
+            {
+                mode = Mode.Console;
+            }
+            else if (args.Length > 0)
+            {
+                mode = Mode.File;
+            }
+            else
+            {
+                Console.Error.WriteLine("No input provided.");
+                return -1;
+            }
+
+            string input = await GetInput(mode, args).ConfigureAwait(false);
+
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(input);
             CompilationUnitSyntax root = syntaxTree.GetCompilationUnitRoot();
-            var organizedRoot = root
+
+            var organizedRoot = Reorganize(root);
+
+            await SetOuput(organizedRoot, mode, args).ConfigureAwait(false);
+            return 0;
+        }
+
+        private static async Task<string> GetInput(Mode mode, string[] args)
+        {
+            switch (mode)
+            {
+                case Mode.Console:
+                    using (var reader = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding))
+                    {
+                        return await reader.ReadToEndAsync().ConfigureAwait(false);
+                    }
+
+                case Mode.File:
+                    return await File.ReadAllTextAsync(args[0]).ConfigureAwait(false);
+
+                default:
+                    throw new NotImplementedException($"Mode \"{mode}\" not implemented.");
+            }
+        }
+
+        private static CompilationUnitSyntax Reorganize(CompilationUnitSyntax compilationUnit)
+        {
+            return compilationUnit
                 .WithReorganizedUsings()
                 .WithReorganizeMembers()
                 .WithCorrectSpacing();
-            Console.WriteLine(organizedRoot);
+        }
+
+        private static async Task SetOuput(CompilationUnitSyntax compilationUnit, Mode mode, string[] args)
+        {
+            switch (mode)
+            {
+                case Mode.Console:
+                    Console.Write(compilationUnit.ToFullString());
+                    break;
+
+                case Mode.File:
+                    await File.WriteAllTextAsync(args[0], compilationUnit.ToFullString()).ConfigureAwait(false);
+                    break;
+
+                default:
+                    throw new NotImplementedException($"Mode \"{mode}\" not implemented.");
+            }
         }
     }
 }
