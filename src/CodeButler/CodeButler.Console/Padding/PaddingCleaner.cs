8223fc96155ef21e228334397a9114aac5c30f7e
Sebastian Rumohr <sebastian.rumohr@projektanker.de>
2021-01-31 18:27:03 +0100

    Renames variable

--- src/CodeButler/CodeButler.Console/Padding/PaddingCleaner.cs
+++ src/CodeButler/CodeButler.Console/Padding/PaddingCleaner.cs
@@ -5,12 +5,12 @@ namespace CodeButler.Padding
 {
     public class PaddingCleaner
     {
-        private static readonly Regex _dirtyEmptyLine = new Regex(@"[\t ]+(\r\n|\n)", RegexOptions.Multiline);
+        private static readonly Regex _dirtyNewLine = new Regex(@"[\t ]+(\r\n|\n)", RegexOptions.Multiline);
         private static readonly Regex _multiEmptyLine = new Regex(@"^(\r\n|\n)+", RegexOptions.Multiline);
 
         public string Clean(string input)
         {
-            string output = _dirtyEmptyLine.Replace(input, match => match.Groups.Values.Last().Value);
+            string output = _dirtyNewLine.Replace(input, match => match.Groups.Values.Last().Value);
             output = _multiEmptyLine.Replace(output, match => match.Groups.Values.Last().Value);
             return output;
         }
