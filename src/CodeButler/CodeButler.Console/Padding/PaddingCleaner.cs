f1d4fcf4d18cd2b21e31ede858cf2c80654f0428
just-seba <46223600+just-seba@users.noreply.github.com>
2024-03-02 13:14:47 +0100

    refactor: `PaddingCleaner` uses `GeneratedRegexAttribute`.

--- src/CodeButler/CodeButler.Console/Padding/PaddingCleaner.cs
+++ src/CodeButler/CodeButler.Console/Padding/PaddingCleaner.cs
@@ -1,18 +1,22 @@
-﻿using System.Linq;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
-namespace CodeButler.Padding
+namespace CodeButler.Padding;
+
+public partial class PaddingCleaner
 {
-    public class PaddingCleaner
-    {
-        private static readonly Regex _dirtyNewLine = new Regex(@"[\t ]+(\r\n|\n)", RegexOptions.Multiline);
-        private static readonly Regex _multiEmptyLine = new Regex(@"^(\r\n|\n)+", RegexOptions.Multiline);
+    private static readonly Regex _dirtyNewLine = GenerateDirtyNewLineRegex();
+    private static readonly Regex _multiEmptyLine = GenerateMultiEmptyLineRegex();
 
-        public string Clean(string input)
-        {
-            string output = _dirtyNewLine.Replace(input, match => match.Groups.Values.Last().Value);
-            output = _multiEmptyLine.Replace(output, match => match.Groups.Values.Last().Value);
-            return output;
-        }
+    public virtual string Clean(string input)
+    {
+        string output = _dirtyNewLine.Replace(input, match => match.Groups[^1].Value);
+        output = _multiEmptyLine.Replace(output, match => match.Groups[^1].Value);
+        return output;
     }
+
+    [GeneratedRegex(@"[\t ]+(\r\n|\n)", RegexOptions.Multiline)]
+    private static partial Regex GenerateDirtyNewLineRegex();
+
+    [GeneratedRegex(@"^(\r\n|\n)+", RegexOptions.Multiline)]
+    private static partial Regex GenerateMultiEmptyLineRegex();
 }
