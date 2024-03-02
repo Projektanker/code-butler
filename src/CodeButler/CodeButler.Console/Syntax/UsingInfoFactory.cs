4ae4ad39038c36b76f4bbb3e90ed6176feb7751e
just-seba <46223600+just-seba@users.noreply.github.com>
2024-03-02 13:49:24 +0100

    chore: Update NuGet packages, fixes new warnings.

--- src/CodeButler/CodeButler.Console/Syntax/UsingInfoFactory.cs
+++ src/CodeButler/CodeButler.Console/Syntax/UsingInfoFactory.cs
@@ -7,7 +7,7 @@ public static class UsingInfoFactory
 {
     public static UsingInfo GetUsingInfo(this UsingDirectiveSyntax usingDirective)
     {
-        return new UsingInfo(usingDirective.Name.ToString())
+        return new UsingInfo(usingDirective.Name?.ToString() ?? string.Empty)
         {
             Alias = usingDirective.Alias?.Name.ToString(),
             IsStatic = usingDirective.StaticKeyword != default,
